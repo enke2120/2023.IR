3c33a3cbe3156a1c03c1298d5993a477f21fb3d2
kojunpyo2120 <54061736+enke2120@users.noreply.github.com>
2023-08-13 21:53:40 +0900

    Update Client.cs

--- Client.cs
+++ Client.cs
@@ -6,13 +6,16 @@ public class Client : HNET.CHNetConnector
 {
     public bool connect = false;
     public bool already_started = false;
+    public bool accessible = false;
+    public bool already_jumped = false;
     public Vector3 vecDir;
+    public GameObject player;
+    public float Ydir;
     public float temp_angle;
     public float current_angle;
-    public GameObject player;
     public float[] starts = new float[2]{0.0f, 0.0f};
     public float[] temp_starts = new float[2]{0.0f, 0.0f};
-    public bool accessible = false;
+
     public override void OnConnect()
     {
         UnityEngine.Debug.Log("▶ connect");
@@ -21,49 +24,6 @@ public class Client : HNET.CHNetConnector
     public override void OnDisconnect()
     {
         UnityEngine.Debug.Log("▶ disconnect");
-    }
-
-    // 패킷 수신 함수
-    public override void OnMessage(HNetPacket Packet)
-    {
-        // if (Packet.Type() == 666) {
-
-        //     // 패킷에서 정보 얻기.
-        //     float a1 = 0.0f;
-        //     float a2 = 0.0f;
-        //     float a3 = 0.0f;
-        //     // float b1 = 0.0f;
-        //     // float b2 = 0.0f;
-        //     // float b3 = 0.0f;
-        //     Packet.Out(ref a1); //
-        //     Packet.Out(ref a2); // → 서버의 패킷 수신
-        //     Packet.Out(ref a3); //
-        //     // Packet.Out(ref b1); //
-        //     // Packet.Out(ref b2); //
-        //     // Packet.Out(ref b3); //
-
-        //     // 정보 출력.
-        //     // string s = string.Format("▶ ({0}, {1}, {2})", a1, a2, a3); // 유니티 콘솔에 패킷 출력
-        //     // UnityEngine.Debug.Log(s);
-
-        // } else if (Packet.Type() == 888) {
-
-        //     // 패킷에서 정보 얻기.
-        //     float a1 = 0.0f;
-        //     float a2 = 0.0f;
-        //     float a3 = 0.0f;
-        //     Packet.Out(ref a1); //
-        //     Packet.Out(ref a2); // → 서버의 패킷 수신
-        //     Packet.Out(ref a3); //
-
-        //     // 정보 출력.
-        //     // string s = string.Format("▶ ({0}, {1}, {2}, {3}, {4}, {5})", a1, a2, a3, b1, b2, b3); // 유니티 콘솔에 패킷 출력
-        //     // UnityEngine.Debug.Log(s);
-        // } else {
-        //     return;
-        // }
-
-        return;
     }
 
     // 유니티 게임 화면의 버튼 기능 함수
@@ -89,35 +49,38 @@ public class Client : HNET.CHNetConnector
         if (GUI.Button(new Rect(200, 340, 300, 100), "보내기"))
         {
             // 이미 보내는 중이 아니라면
-            if (already_started == false) {
+            if (already_started == false)
+            {
                 accessible = true;
-                InvokeRepeating("transmit_position", 0f, 0.2f);
-                InvokeRepeating("transmit_direction", 0f, 0.2f);
+                InvokeRepeating("transmit_position", 0f, 1.0f);
+                //InvokeRepeating("transmit_direction", 0f, 0.2f);
                 already_started = true; // 이미 보내는 중이라 저장
             } else { // 이미 보내는 중이라면
-                CancelInvoke("transmit_position"); // 패킷 전송 중지
+                //CancelInvoke("transmit_position"); // 패킷 전송 중지
                 already_started = false; // 이미 보내는 중이 아니라 저장
             }
-
-            // InvokeRepeating("continuous_Update", 0f, 0.1f);
         }
     }
 
-    public void FixedUpdate() { // 주기적으로 반복되는 함수. → 여기서는 데드레커닝 기법을 구현한 지점.
-     
-        starts[0] = Input.GetAxisRaw("Vertical");
-        starts[1] = Input.GetAxisRaw("Horizontal");
-    
-        transmit_direction();
+    public void FixedUpdate() // 주기적으로 반복되는 함수. → 여기서는 데드레커닝 기법을 구현한 지점.
+    { 
+        if (Input.GetButton("Jump") && already_jumped == false)
+        {
+            transmit_jump();
+        }
+        
+        if (starts[0] != Input.GetAxisRaw("Vertical") || starts[1] != Input.GetAxisRaw("Horizontal"))
+        {
+            transmit_direction();
+            starts[0] = Input.GetAxisRaw("Vertical");
+            starts[1] = Input.GetAxisRaw("Horizontal");
+        }
 
         current_angle = player.transform.eulerAngles.y;
 
-        if (Math.Abs(temp_angle - current_angle) > 2) {
-            // Debug.Log("Angle exceeded");
-
+        if (Math.Abs(temp_angle - current_angle) > 4)
+        {
             temp_angle = current_angle;
-
-            accessible = true; // 스타트 값에 변화를 주어, transmit_direction이 패킷을 보내도록 유도
             transmit_direction();
         }
     }
@@ -125,20 +88,17 @@ public class Client : HNET.CHNetConnector
     // 패킷 송신 함수
     public void transmit_position()
     {
-        // 777 타입의 패킷 송신
-        HNET.NewPacket Out = new HNET.NewPacket(777);
-
-        Vector3 pos; // 플레이어 오브젝트의 위치 저장 변수 (x, y, z)
-        pos = this.gameObject.transform.position; // 변수에 위치 대입
+        HNET.NewPacket Out = new HNET.NewPacket(777); // 777 타입의 패킷 송신
 
         // 플레이어 오브젝트의 위치를 송신
-        Out.In(pos.x);
-        Out.In(pos.y);
-        Out.In(pos.z);
+        Out.In(player.transform.position.x);
+        Out.In(player.transform.position.y);
+        Out.In(player.transform.position.z);
         Send(Out);
 
         // 연결이 '거짓'이면
-        if (connect == false) {
+        if (connect == false)
+        {
             CancelInvoke("transmit_position"); // 패킷 전송 중지
             already_started = false; // 이미 보내는 중이 아니라 저장
         }
@@ -147,33 +107,31 @@ public class Client : HNET.CHNetConnector
     // 패킷 송신 함수
     public void transmit_direction()
     {
-        if (temp_starts[0] != starts[0] || temp_starts[1] != starts[1] || accessible == true) {
-            // Debug.Log("difference detacted");
+        // 999 타입의 패킷 송신
+        HNET.NewPacket Out = new HNET.NewPacket(999);
 
-            // 999 타입의 패킷 송신
-            HNET.NewPacket Out = new HNET.NewPacket(999);
+        vecDir.x = Input.GetAxisRaw("Horizontal");
+        vecDir.z = Input.GetAxisRaw("Vertical");
+        vecDir = player.transform.TransformDirection(vecDir);
+        vecDir *= 5.0f;
 
-            if (starts[0] != 0 || starts[1] != 0) {  
-                vecDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-                vecDir = player.transform.TransformDirection(vecDir);
-                vecDir *= 5.0f;
-            } else {
-                Debug.Log("stop");
-                vecDir = new Vector3(0, 0, 0);
-            }
+        // 플레이어 오브젝트의 방향을 송신
+        Out.In(vecDir.x);
+        Out.In(vecDir.z);
+        Send(Out);
+        
+        temp_angle = player.transform.eulerAngles.y;
 
-            // 플레이어 오브젝트의 위치를 송신
-            Out.In(vecDir.x);
-            Out.In(vecDir.y);
-            Out.In(vecDir.z);
-            Send(Out);
-
-            temp_starts[0] = starts[0];
-            temp_starts[1] = starts[1];
-            
-            temp_angle = player.transform.eulerAngles.y;
-
-            accessible = false;
+        // 연결이 '거짓'이면
+        if (connect == false)
+        {
+            CancelInvoke("transmit_position"); // 패킷 전송 중지
         }
+    }
+
+    public void transmit_jump()
+    {
+        HNET.NewPacket Out = new HNET.NewPacket(555); // 555 <-> 444
+        Send(Out);
     }
 }
