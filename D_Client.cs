e6ff7a668a06a53ac1e91f81cdb7a142c0a5a1f5
kojunpyo2120 <54061736+enke2120@users.noreply.github.com>
2023-08-17 17:18:49 +0900

    Update D_Client.cs

--- D_Client.cs
+++ D_Client.cs
@@ -2,67 +2,60 @@ using System;
 using System.IO;
 using UnityEngine;
 
-public class D_Client : HNET.CHNetConnector // 클라이언트 정의
+public class D_Client : HNET.CHNetConnector // 클라이언트 클래스 정의(HNetwork 클래스 상속)
 {
-    public Vector3 vecDir;
-    public Vector3 pos;
+    public bool connect;
+    public int direction_packet_count;
+    
+    public Vector3 PlayerPosition;
     public bool jumping;
-    public bool connect = false;
-    public bool already_started = false;
-    public float a1 = 0.0f;
-    public float a2 = 0.0f;
-    public float a3 = 0.0f;
-    public float b1 = 0.0f;
-    public float b2 = 0.0f;
+
+    public float received_x;
+    public float received_y;
+    public float received_z;
+
+    public float delta_x;
+    public float delta_y;
+    public float delta_z;
     public int count;
-    public float constant;
-    public int packet_count;
 
     string fullpth = "Assets/Others/test1";
     StreamWriter sw;
 
     public void Start() {
-        count = 0;
-        constant = 0;
-
         if (false == File.Exists(fullpth)) {
             sw = new StreamWriter(fullpth + ".txt");
         }
     }
     
-    public override void OnConnect()    // 서버와 연결했을 때 실행
-    {
-        UnityEngine.Debug.Log("▶ connect"); // 콘솔에 출력
-    }
     public override void OnDisconnect() // 서버와의 연결을 해제했을 때 실행
     {
-        sw.WriteLine(packet_count);
+        sw.WriteLine(direction_packet_count);
         sw.Flush();
         sw.Close();
-        UnityEngine.Debug.Log("▶ disconnect");  // 콘솔에 출력
     }
     public override void OnMessage(HNetPacket Packet)   // 패킷 수신 함수
     {
         if (Packet.Type() == 888 && jumping == false) // 패킷 타입이 888일 때
         { 
-            packet_count += 1;
-            Packet.Out(ref b1); //
-            Packet.Out(ref b2); // → 서버의 패킷 수신
+            direction_packet_count += 1;
+            Packet.Out(ref delta_x); //
+            Packet.Out(ref delta_z); // → 서버의 패킷 수신
         }
         else if (Packet.Type() == 666) // 패킷 타입이 666일 때
         {
-            Packet.Out(ref a1); //
-            Packet.Out(ref a2); // → 서버의 패킷 수신
-            Packet.Out(ref a3); //
-            pos = gameObject.transform.position;
-            sw.WriteLine("pred: " + pos.x + ", " + pos.y + ", " + pos.z);
-            gameObject.transform.position = new Vector3(a1, a2, a3);
-            sw.WriteLine("real: " + a1 + ", " + a2 + ", " + a3);
+            Packet.Out(ref received_x); //
+            Packet.Out(ref received_y); // → 서버의 패킷 수신
+            Packet.Out(ref received_z); //
+            PlayerPosition = gameObject.transform.position;
+            sw.WriteLine("pred: " + PlayerPosition.x + ", " + PlayerPosition.y + ", " + PlayerPosition.z);
+            gameObject.transform.position = new Vector3(received_x, received_y, received_z);
+            sw.WriteLine("real: " + received_x + ", " + received_y + ", " + received_z);
         }
         else if (Packet.Type() == 444) {
             if (jumping == false) {
                 jumping = true;
-                constant = 5f;
+                delta_y = 5f;
             }            
         }
         else// 패킷 타입이 666도 888도 아닐 때
@@ -76,19 +69,19 @@ public class D_Client : HNET.CHNetConnector // 클라이언트 정의
         if (jumping == true) {
             if (count < 50)
             {
-                constant -= 0.2f;
+                delta_y -= 0.2f;
             } 
             else
             {
-                constant = 0;
+                delta_y = 0;
                 count = 0;
-                b1 = 0;
-                b2 = 0;
+                delta_x = 0;
+                delta_z = 0;
                 jumping = false;
             }
             count += 1;        
         }
-        gameObject.transform.Translate((new Vector3(b1, constant, b2)) * Time.deltaTime); // 오브젝트의 이동 경로를 방향 벡터로 지정
+        gameObject.transform.Translate((new Vector3(delta_x, delta_y, delta_z)) * Time.deltaTime); // 오브젝트의 이동 경로를 방향 벡터로 지정
     }
 
     public void OnGUI() // 유니티 게임 화면의 버튼 기능 함수
